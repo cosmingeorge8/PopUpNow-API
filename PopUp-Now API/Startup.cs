550a612078746848dc9340b4d145a9ca5997eed6
George Cosmin Mucalau <459623@student.saxion.nl>
2022-04-17 19:29:29 +0300

    - added open api spec

--- PopUp-Now API/Startup.cs
+++ PopUp-Now API/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using PopUp_Now_API.Database;
 using PopUp_Now_API.Interfaces;
 using PopUp_Now_API.Model;
@@ -79,12 +80,20 @@ namespace PopUp_Now_API
             });
 
             services.AddHealthChecks();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Pop-up Now API", Version = "v1"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, UserManager<User> userManager,
             RoleManager<IdentityRole> roleManager, DataContext dataContext)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pop-UP NOW v1"));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
