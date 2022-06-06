365b337eff6a618dcc18c7f2d35110d31eee6ca4
cosmin.george8 <459623@student.saxion.nl>
2022-06-06 19:16:59 +0200

    - added comments in ErrorHandler.cs

--- PopUp-Now API/Exceptions/ErrorHandler.cs
+++ PopUp-Now API/Exceptions/ErrorHandler.cs
@@ -7,6 +7,10 @@ using SendGrid.Helpers.Errors.Model;
 
 namespace PopUp_Now_API.Exceptions
 {
+    /**
+     * Error handler class
+     * If an error is being thrown before the response is created, will be handled by this middleware
+     */
     public class ErrorHandler
     {
         private readonly RequestDelegate _requestDelegate;
@@ -16,12 +20,18 @@ namespace PopUp_Now_API.Exceptions
             _requestDelegate = requestDelegate;
         }
 
+        /**
+         * Method called after a HTTP request is sent.
+         * Handles exceptions that could be thrown while processing the request
+         */
         public async Task Invoke(HttpContext context)
         {
             try
             {
+                //try to process the request
                 await _requestDelegate(context);
             }
+            //if an exception occurs, format a proper response with an HTTP Status Code and a message
             catch (Exception exception)
             {
                 var response = context.Response;
